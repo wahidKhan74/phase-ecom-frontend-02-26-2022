ed1e3cac06b9175105796a2fc753ed0918cd660f
waheed khan <wahid---@gmail.com>
2022-02-27 23:55:16 +0530

    added template form

--- src/app/component/register/register.component.ts
+++ src/app/component/register/register.component.ts
@@ -7,9 +7,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  public user: User = {
+    firstName:'',
+    lastName:"",
+    password:'',
+    email:'',
+    state:'',
+    zipcode:0,
+    agree:false
+  }
+  public submitted:boolean = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  onSubmit(form:any) {
+    console.log("The form has submited");
+    if(form.valid){
+      this.submitted = true;
+      console.log(this.user);
+    } else{
+      this.validate(form);
+    }
+    
+  }
+
+  hasError(field:any) {
+    return (field.invalid && field.touched && field.errors);
+  }
+
+  validate(form:any){
+    console.log(form);
+    
+    Object.keys(form.controls).forEach(field =>{
+      const control = form.controls[field];
+      control.markAsTouched({ onlySelft : true});
+    })
+  }
+
+}
+
+interface User {
+  firstName:string;
+  lastName:string;
+  password:string;
+  email:string;
+  state:string;
+  zipcode:number;
+  agree:boolean;
 }
